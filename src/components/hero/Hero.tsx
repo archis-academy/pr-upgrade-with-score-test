4f039218225a00d6da105c033e7bdc9328b6369f
midlaj <mumidlaj@gmail.com>
2025-10-15 01:08:46 +0530

    [PR-1] - Instead direct harcoding we moved into prop to handle in main component

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -2,19 +2,19 @@ import "./Hero.scss";
 
 interface HeroProps {
   onCtaClick: () => void;
+  title: string;
+  subtitle: string;
+  ctaText: string;
 }
 
-const Hero = ({ onCtaClick }: HeroProps) => {
+const Hero = ({ onCtaClick, title, subtitle, ctaText }: HeroProps) => {
   return (
     <section className="hero">
       <div className="container">
-        <h1 className="hero__title">Product Launch 2025</h1>
-        <p className="hero__subtitle">
-          Discover the next generation of innovation. Sign up for exclusive
-          updates!
-        </p>
+        <h1 className="hero__title">{title}</h1>
+        <p className="hero__subtitle">{subtitle}</p>
         <button className="hero__cta" onClick={onCtaClick}>
-          Learn More & Sign Up
+          {ctaText}
         </button>
       </div>
     </section>
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -11,7 +11,12 @@ function Home() {
 
   return (
     <main>
-      <Hero onCtaClick={handleScrollToSignup} />
+      <Hero
+        onCtaClick={handleScrollToSignup}
+        title="Product Launch 2025"
+        subtitle="Discover the next generation of innovation. Sign up for exclusive updates!"
+        ctaText="Learn More & Sign Up"
+      />
       <SignupForm ref={signupFormRef} />
     </main>
   );
