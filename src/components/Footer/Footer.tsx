4fd946968977efe3597936e7089897a21ceef4e0
midlaj <mumidlaj@gmail.com>
2025-10-22 00:47:29 +0530

    [PR-2] - Added LLC

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-900 text-gray-400 p-4 text-center">
       <p className="flex items-center justify-center gap-2 m-0">
-        <span>© 2025 Archi's Academy. All Rights Reserved.</span>
+        <span>© 2025 Archi's Academy LLC. All Rights Reserved.</span>
         <FaHeart className={styles.footerIcon} />
       </p>
     </footer>
