0864ffcc07eeb96fb4e3200159c7526a6c30cfb9
Muhammed Midlaj <70736601+miidlaj@users.noreply.github.com>
2025-10-30 14:57:08 +0530

    Revert "[PR-1] - Landing page"

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,25 +1,7 @@
-import { useRef } from "react";
-import Hero from "../../components/hero/Hero";
-import SignupForm from "../../components/signup/SignupForm";
+import Welcome from "@components/welcome/welcome";
 
 function Home() {
-  const signupFormRef = useRef<HTMLElement>(null);
-
-  const handleScrollToSignup = () => {
-    signupFormRef.current?.scrollIntoView({ behavior: "smooth" });
-  };
-
-  return (
-    <main>
-      <Hero
-        onCtaClick={handleScrollToSignup}
-        title="Product Launch 2025"
-        subtitle="Discover the next generation of innovation. Sign up for exclusive updates!"
-        ctaText="Learn More & Sign Up"
-      />
-      <SignupForm ref={signupFormRef} />
-    </main>
-  );
+  return <Welcome />;
 }
 
 export default Home;
